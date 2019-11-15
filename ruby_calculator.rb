2283aa4f65a2fac65b9bfaae7e9d828506e821c0
Gabriella Algecera <gabby.algecera@gmail.com>
2019-11-14 18:28:43 -0700

    fix to number variables

--- ruby_calculator.rb
+++ ruby_calculator.rb
@@ -1,43 +1,47 @@
-# take the first number
-# take modifier
-# take the last number
-# puts@result
-# error checking for user input
-# user input and modifiers should be checked for proper input type
-# the@result should set  @first_modifier and allow for another modifier and last number so@result continues +
-# + to build until the user decides to clear the calculator
-# - make + - * / work
-# - make clear work
 
 def menu
     puts "This is a calculator"
     puts "What is the first number?"
-    first_modifier = gets.strip
-    if first_modifier =~ /\D/
-        error_input
-    end
+    first = first_number
     puts "What is the operator?"
     operator = gets.strip.to_s
     puts "What is the second number?"
-    second_modifier = gets.strip
-    if second_modifier =~ /\D/
-        error_input
-    end
-    calculating
-
+    second = second_number  
+    puts "Calculating..."
+    
     case operator
     when '+'
-        @result = first_modifier.to_f + second_modifier.to_f
+        @result = first.to_f + second.to_f
     when '-'
-        @result = first_modifier.to_f - second_modifier.to_f
+        @result = first.to_f - second.to_f
     when '/'
-        @result = first_modifier.to_f / second_modifier.to_f
+        @result = first.to_f / second.to_f
     when '*'
-        @result = first_modifier.to_f * second_modifier.to_f
+        @result = first.to_f * second.to_f
+    when 'Clear'
+        goodbye
     else 
       puts "This calculator does not support that as an operator"
       puts "Please use +, -, /, or *"
       menu
+    end
+end
+
+def first_number
+    first_modifier = gets.strip  
+    if first_modifier =~ /\D/
+        error_input
+    else 
+        return first_modifier
+    end
+end
+
+def second_number
+    second_modifier = gets.strip
+    if second_modifier =~ /\D/
+        error_input
+    else 
+        return second_modifier
     end
 end
 
@@ -47,12 +51,7 @@ def error_input
     menu
 end
 
-def calculating
-   puts "Calculating..."
-end
-
 menu
-calculating
 puts @result
 
 
